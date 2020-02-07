27bc28c341c088ff4bfc4a66f48255f807227086
Joris Demandre <joris.demandre@agence-soon.fr>
2020-02-07 12:16:58 +0100

    add frontend module and network

--- main.tf
+++ main.tf
@@ -18,6 +18,13 @@ module "back_end" {
   public_subnet_depends_on = [aws_internet_gateway.main]
 }
 
+module "front_end" {
+  source = "./modules/front_end"
+  GITHUB_ACCESS_TOKEN = var.GITHUB_ACCESS_TOKEN
+  vpc = aws_vpc.front
+  public_subnet_depends_on = [aws_internet_gateway.front]
+}
+
 module "serverless_app" {
   source = "./modules/serverless_app"
   sqs_id = module.sqs_event_service.sqs_id
@@ -33,4 +40,10 @@ output "serverless_app_url" {
 output "back_end_app_url" {
   value = module.back_end.alb_hostname
   description = "The url for the back end"
+}
+
+
+output "front_end_app_url" {
+  value = module.front_end.alb_hostname
+  description = "The url for the front end"
 }
--- network.tf
+++ network.tf
@@ -2,12 +2,26 @@ resource "aws_vpc" "main" {
   cidr_block = "172.17.0.0/16"
 }
 
+resource "aws_vpc" "front" {
+  cidr_block = "172.18.0.0/16"
+}
+
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
+}
+
+resource "aws_internet_gateway" "front" {
+  vpc_id = aws_vpc.front.id
 }
 
 resource "aws_route" "main" {
   route_table_id         = aws_vpc.main.main_route_table_id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.main.id
+}
+
+resource "aws_route" "front" {
+  route_table_id         = aws_vpc.front.main_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.front.id
 }
