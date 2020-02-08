d538c51e50cc0918920266086209b94f9415c8f7
Mathieu <mathieu.0598@gmail.com>
2020-02-08 22:28:31 +0100

    Use generic module for back end and front end

--- main.tf
+++ main.tf
@@ -3,26 +3,61 @@ provider "aws" {
     region = "eu-west-3"
 }
 
+resource "aws_dynamodb_table" "back_end_product_table" {
+  name           = "Product"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "Id"
+
+  attribute {
+    name = "Id"
+    type = "S"
+  }
+}
 
 module "sqs_event_service" {
   source = "./modules/sqs_event_service"
-  
 }
 
 module "back_end" {
-  source = "./modules/back_end"
-  GITHUB_ACCESS_TOKEN = var.GITHUB_ACCESS_TOKEN
+  source = "./modules/ecs_ec2_codepipeline"
+  prefix = "esgi-cloud-back-end"
+  private_subnet = aws_subnet.private
+  public_subnet = aws_subnet.public
+  availability_zone = data.aws_availability_zones.available.names
+  az_count = var.az_count
+  buildspec_path = "${path.module}/conf/back_end/buildspec.yml"
+  task_definition_path = "${path.module}/conf/back_end/task_definitions_service.json"
   vpc = aws_vpc.main
   sqs_id = module.sqs_event_service.sqs_id
   sqs_arn = module.sqs_event_service.sqs_arn
   public_subnet_depends_on = [aws_internet_gateway.main]
+  code_pipeline_source_conf = {
+    OAuthToken = var.GITHUB_ACCESS_TOKEN
+    Owner  = "esgi-cloud-project"
+    Repo   = "back_end_app"
+    Branch = "master"
+  }
 }
 
 module "front_end" {
-  source = "./modules/front_end"
-  GITHUB_ACCESS_TOKEN = var.GITHUB_ACCESS_TOKEN
-  vpc = aws_vpc.front
-  public_subnet_depends_on = [aws_internet_gateway.front]
+  source = "./modules/ecs_ec2_codepipeline"
+  prefix = "esgi-cloud-front-end"
+  private_subnet = aws_subnet.private
+  public_subnet = aws_subnet.public
+  availability_zone = data.aws_availability_zones.available.names
+  az_count = var.az_count
+  buildspec_path = "${path.module}/conf/front_end/buildspec.yml"
+  task_definition_path = "${path.module}/conf/front_end/task_definitions_service.json"
+  vpc = aws_vpc.main
+  sqs_id = module.sqs_event_service.sqs_id
+  sqs_arn = module.sqs_event_service.sqs_arn
+  public_subnet_depends_on = [aws_internet_gateway.main]
+  code_pipeline_source_conf = {
+    OAuthToken = var.GITHUB_ACCESS_TOKEN
+    Owner  = "esgi-cloud-project"
+    Repo   = "front_end_app"
+    Branch = "master"
+  }
 }
 
 module "serverless_app" {
--- network.tf
+++ network.tf
@@ -3,17 +3,17 @@ resource "aws_vpc" "main" {
   enable_dns_hostnames = true
 }
 
-resource "aws_vpc" "front" {
-  cidr_block = "172.18.0.0/16"
-}
+# resource "aws_vpc" "front" {
+#   cidr_block = "172.18.0.0/16"
+# }
 
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
 }
 
-resource "aws_internet_gateway" "front" {
-  vpc_id = aws_vpc.front.id
-}
+# resource "aws_internet_gateway" "front" {
+#   vpc_id = aws_vpc.front.id
+# }
 
 resource "aws_route" "main" {
   route_table_id         = aws_vpc.main.main_route_table_id
@@ -21,8 +21,42 @@ resource "aws_route" "main" {
   gateway_id             = aws_internet_gateway.main.id
 }
 
-resource "aws_route" "front" {
-  route_table_id         = aws_vpc.front.main_route_table_id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.front.id
+# resource "aws_route" "front" {
+#   route_table_id         = aws_vpc.front.main_route_table_id
+#   destination_cidr_block = "0.0.0.0/0"
+#   gateway_id             = aws_internet_gateway.front.id
+# }
+
+data "aws_availability_zones" "available" {
+}
+
+resource "aws_subnet" "private" {
+  count             = var.az_count
+  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  vpc_id            = aws_vpc.main.id
+}
+
+resource "aws_subnet" "public" {
+  count                   = var.az_count
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, var.az_count + count.index)
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  vpc_id                  = aws_vpc.main.id
+  map_public_ip_on_launch = false
+}
+
+resource "aws_route_table" "private" {
+  count  = var.az_count
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    instance_id = element(aws_instance.NatInstance.*.id, count.index)
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  count          = var.az_count
+  subnet_id      = element(aws_subnet.private.*.id, count.index)
+  route_table_id = element(aws_route_table.private.*.id, count.index)
 }
