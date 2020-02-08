206daa0baba9030acb50fe4fde67f14a6aa586d2
Mathieu <mathieu.0598@gmail.com>
2020-02-08 17:57:53 +0100

    Update network conf and back end configuration

--- network.tf
+++ network.tf
@@ -1,10 +1,12 @@
 resource "aws_vpc" "main" {
   cidr_block = "172.17.0.0/16"
+  enable_dns_hostnames = true
 }
 
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
 }
+
 
 resource "aws_route" "main" {
   route_table_id         = aws_vpc.main.main_route_table_id
